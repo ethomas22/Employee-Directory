86498535720a22440586a842daff66de61b1bd22
Emily Thomas <ethomas@quint.co.uk>
2021-02-24 20:35:42 -0800

    added components folder and corresponding js files

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function header() {
+  return (
+    <div>
+      <h1>Employee Directory</h1>
+      <div>
+        Click on carrots to filter by heading or use the search box to narrow
+        your results.
+      </div>
+    </div>
+  );
+}
+
+export default header;
